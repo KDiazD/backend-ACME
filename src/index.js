4641fe3e6a9deb896fe1bef6da340cb36a4659f3
Karla Diaz <karladiaz695@gmail.com>
2023-01-19 22:47:19 -0500

    configuración index

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+
+//asignar puerto
+app.set("port", process.env.PORT || 8000);
+
+//peticiones o middlewares
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//variables globales
+app.use((req, res, next) => {
+  next();
+});
+
+//rutas
+
+
+app.use("/asesorescomerciales", require("./routes/api/usuarios"));
+app.use("/comisiones", require("./routes/api/comentarios"));
+
+
+//public
+app.use(express.static(path.join(__dirname, "public")));
+
+// inicia servidor
+app.listen(app.get("port"), () => {
+  console.log("server on port", app.get("port"));
+});

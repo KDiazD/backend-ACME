aa35e178dde93214342c1b834e3cbc858c30e2fe
Karla Diaz <karladiaz695@gmail.com>
2023-01-24 11:56:58 -0500

    Envío id de asesor para editar o eliminar

--- src/models/asesoresModel.js
+++ src/models/asesoresModel.js
@@ -4,7 +4,7 @@ const model = {};
 //Método para listar asesores
 
 model.listar_asesores = async () => {
-    const sql = `SELECT asesores.nombre AS Nombre_asesor, ne.nombre AS Nivel_de_Experiencia 
+    const sql = `SELECT asesores.id, asesores.nombre AS Nombre_asesor, ne.nombre AS Nivel_de_Experiencia 
     FROM asesores AS asesores, nivel_experiencia AS ne 
     WHERE asesores.nivel_experiencia_id = ne.id AND estado != 2`;
     const asesores = await pool.query(sql);
